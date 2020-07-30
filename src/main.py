b771e7e91e16073fd911bd93a3476d1f9c270715
WolfenCLI <wolfenCLI@zohomail.eu>
2020-07-30 18:15:48 +0200

    switches order of cog addition

--- src/main.py
+++ src/main.py
@@ -6,6 +6,6 @@ from cogs import PostManager, Utils
 
 if __name__ == "__main__":
     bot = commands.Bot(command_prefix='$')
-    bot.add_cog(PostManager(bot))
     bot.add_cog(Utils(bot))
+    bot.add_cog(PostManager(bot))
     bot.run(os.getenv("BOT_TOKEN"))
