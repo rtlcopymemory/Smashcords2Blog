76f2d68b8ccba7d372be412503d6e051839528f8
WolfenCLI <wolfenCLI@zohomail.eu>
2020-08-01 12:32:47 +0200

    implements remove category

--- Smashcords2Blog/cogs/Posts.py
+++ Smashcords2Blog/cogs/Posts.py
@@ -12,9 +12,9 @@ class Posts(commands.Cog):
         self.bot: Smashcords2BlogBot = bot
         self.temp_posts: dict = {}  # Dictionary mapping server id -> current embed
 
-    @commands.command(name='createpost', usage="",
+    @commands.command(name='newpost', usage="",
                       brief="Initiates a new post",
-                      aliases=['newpost', 'new'])
+                      aliases=['new'])
     async def create_embed(self, ctx: commands.Context):
         if not is_mod(self.bot.conn, ctx):
             await ctx.send("You're not a mod")
@@ -25,10 +25,11 @@ class Posts(commands.Cog):
     @commands.command(name='createcategory', usage="name",
                       brief="Creates a new category",
                       aliases=['newcategory', 'newcat', 'addcat', 'addcategory'])
-    async def create_category(self, ctx: commands.Context, arg: str):
+    async def create_category(self, ctx: commands.Context, *args: str):
         if not is_mod(self.bot.conn, ctx):
             await ctx.send("You're not a mod")
             return
+        arg: str = ' '.join(args)
         try:
             categories.add_category(self.bot.conn, ctx.guild.id, arg)
         except psycopg2.IntegrityError as err:
@@ -37,8 +38,8 @@ class Posts(commands.Cog):
             return
         await ctx.send("Category `{}` successfully created".format(arg))
 
-    @commands.command(name='listcategories', usage="name",
-                      brief="Creates a new category",
+    @commands.command(name='listcategories', usage="",
+                      brief="Lists all categories",
                       aliases=['listcat', 'lc', 'categories'])
     async def list_categories(self, ctx: commands.Context):
         if not is_mod(self.bot.conn, ctx):
@@ -49,3 +50,18 @@ class Posts(commands.Cog):
         for category in categories_list:
             message += "{}\n".format(category)
         await ctx.send(message)
+
+    @commands.command(name='removecategory', usage="name",
+                      brief="Removes a category",
+                      aliases=['deletecategory', 'rmcat', 'yeetcat'])
+    async def remove_category(self, ctx: commands.Context, *args: str):
+        if not is_mod(self.bot.conn, ctx):
+            await ctx.send("You're not a mod")
+            return
+        arg: str = ' '.join(args)
+        categories_list: list = categories.get_server_categories(self.bot.conn, ctx.guild.id)
+        if arg not in categories_list:
+            await ctx.send("Category does not exist")
+            return
+        categories.remove_category(self.bot.conn, ctx.guild.id, arg)
+        await ctx.send("Category `{}` successfully removed".format(arg))
--- Smashcords2Blog/database/categories.py
+++ Smashcords2Blog/database/categories.py
@@ -1,4 +1,4 @@
-def add_category(conn, server_id: str, category_name):
+def add_category(conn, server_id: str, category_name: str):
     curr = conn.cursor()
     curr.execute("INSERT INTO smashcords2blog.category (serverid, catname) VALUES (%s, %s)", (server_id, category_name))
     conn.commit()
@@ -11,3 +11,11 @@ def get_server_categories(conn, server_id) -> list:
     result = curr.fetchall()
     curr.close()
     return [role_tuple[0] for role_tuple in result]
+
+
+def remove_category(conn, server_id: int, category_name: str):
+    curr = conn.cursor()
+    curr.execute("DELETE FROM smashcords2blog.category WHERE serverid = %s AND catname = %s",
+                 (server_id, category_name))
+    conn.commit()
+    curr.close()
