4fa1c20abf9b55d6e8fcb90f4c4a57b6715cffda
WolfenCLI <wolfenCLI@zohomail.eu>
2020-08-01 12:20:13 +0200

    implements list category

--- Smashcords2Blog/cogs/Posts.py
+++ Smashcords2Blog/cogs/Posts.py
@@ -1,3 +1,4 @@
+import discord
 import psycopg2
 from discord.ext import commands
 
@@ -9,14 +10,17 @@ from database import categories
 class Posts(commands.Cog):
     def __init__(self, bot):
         self.bot: Smashcords2BlogBot = bot
+        self.temp_posts: dict = {}  # Dictionary mapping server id -> current embed
 
-    @commands.command(name='createpost', usage="category",
+    @commands.command(name='createpost', usage="",
                       brief="Initiates a new post",
                       aliases=['newpost', 'new'])
-    async def create_embed(self, ctx: commands.Context, arg: str):
+    async def create_embed(self, ctx: commands.Context):
         if not is_mod(self.bot.conn, ctx):
             await ctx.send("You're not a mod")
             return
+        self.temp_posts[ctx.guild.id] = discord.Embed(color=discord.Color.from_rgb(106, 252, 228))
+        await ctx.send("New post initiated")
 
     @commands.command(name='createcategory', usage="name",
                       brief="Creates a new category",
@@ -32,3 +36,16 @@ class Posts(commands.Cog):
             self.bot.conn.rollback()
             return
         await ctx.send("Category `{}` successfully created".format(arg))
+
+    @commands.command(name='listcategories', usage="name",
+                      brief="Creates a new category",
+                      aliases=['listcat', 'lc', 'categories'])
+    async def list_categories(self, ctx: commands.Context):
+        if not is_mod(self.bot.conn, ctx):
+            await ctx.send("You're not a mod")
+            return
+        categories_list: list = categories.get_server_categories(self.bot.conn, ctx.guild.id)
+        message: str = "Available categories:\n"
+        for category in categories_list:
+            message += "{}\n".format(category)
+        await ctx.send(message)
--- Smashcords2Blog/database/categories.py
+++ Smashcords2Blog/database/categories.py
@@ -3,3 +3,11 @@ def add_category(conn, server_id: str, category_name):
     curr.execute("INSERT INTO smashcords2blog.category (serverid, catname) VALUES (%s, %s)", (server_id, category_name))
     conn.commit()
     curr.close()
+
+
+def get_server_categories(conn, server_id) -> list:
+    curr = conn.cursor()
+    curr.execute("SELECT catname FROM smashcords2blog.category WHERE serverid = %s", (server_id,))
+    result = curr.fetchall()
+    curr.close()
+    return [role_tuple[0] for role_tuple in result]
