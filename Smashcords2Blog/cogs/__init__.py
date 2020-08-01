a6a72682c7cabef8cfa329cefd1d893d33c8af93
WolfenCLI <wolfenCLI@zohomail.eu>
2020-08-01 11:08:36 +0200

    creates the posts cog

--- Smashcords2Blog/cogs/__init__.py
+++ Smashcords2Blog/cogs/__init__.py
@@ -1,2 +1,3 @@
 from .PermissionsManager import PermissionsManager, is_mod
+from .Posts import Posts
 from .Utils import Utils
--- Smashcords2Blog/main.py
+++ Smashcords2Blog/main.py
@@ -4,6 +4,7 @@ import psycopg2
 
 from Smashcords2BlogBot import Smashcords2BlogBot
 from cogs import PermissionsManager, Utils
+from cogs.Posts import Posts
 
 if __name__ == "__main__":
     connection = psycopg2.connect(dbname=os.getenv("DBNAME"), user=os.getenv("DBUSER"), password=os.getenv("DBPASS"),
@@ -11,4 +12,5 @@ if __name__ == "__main__":
     bot = Smashcords2BlogBot(connection, command_prefix='$')
     bot.add_cog(Utils(bot))
     bot.add_cog(PermissionsManager(bot))
+    bot.add_cog(Posts(bot))
     bot.run(os.getenv("BOT_TOKEN"))
